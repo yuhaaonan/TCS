e607b24b27a8e365b81a84d5a498ec78e2aa3e39
yuhaaonan <m18643950243@163.com>
2024-02-10 14:54:03 +0800

    remove fontend

---
+++ ServerBackend/mian.lua
@@ -0,0 +1,14 @@
+local component = require("component")
+local thread = require("thread")
+local modem = component.modem
+if filesystem.exists("/etc/TCS-SF.conf") then
+    dofile("/etc/TCS-SF.conf")
+else
+    local cfg_port=8888
+    local cfg_timeout=10
+end
+modem.open(cfg_port)
+function message_processer()
+    
+end
+local message_processer_thread = tread.create(message_processer)
